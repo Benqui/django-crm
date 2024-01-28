1466adf56e979c628c781baa626705ab7c7ccea8
Benqui <bennyhoyos@hotmail.com>
2024-01-27 23:01:43 -0600

    modelo implementado y vistas de customer

--- website/urls.py
+++ website/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # path('login/', views.login_usr, name='login'),
     path('logout/', views.logout_usr, name='logout'),
     path('register/', views.register_usr, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
 ]
 
 #no mames esto lo esto haciendo en neovim
--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
+
+
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     # checar si tas logeado o no we 
     if request.method=='POST':
         username = request.POST['usrname']
@@ -17,7 +23,7 @@ def home(request):
             messages.success(request, "Error al loggearte quien ere we!! >:(")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 # def login_usr(request):
 #     pass
@@ -29,4 +35,28 @@ def logout_usr(request):
 
 
 def register_usr(request):
-    return render(request, 'register.html',{})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #auth and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user= authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request, "te registraste correctamente we holi :) ")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html',{'form':form})
+    return render(request, 'register.html',{'form':form})
+
+
+def  customer_record(request, pk):
+    if request.user.is_authenticated:
+        #checar los records
+        cust_record = Record.objects.get(id=pk)
+        return render(request, 'record.html',{'customer_record':cust_record})
+    else:
+        messages.success(request, "Nmms no tas loggiado que haces aqui we >:(")
+        return redirect('home')
