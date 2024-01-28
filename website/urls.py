32f474ba0a0488a2049c8c1bddd8cf4402043e3c
Benqui <bennyhoyos@hotmail.com>
2024-01-27 21:10:33 -0600

    creacion del home, login, logout y registration in process

---
+++ website/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('login/', views.login_usr, name='login'),
+    path('logout/', views.logout_usr, name='logout'),
+    path('register/', views.register_usr, name='register'),
+]
+
+#no mames esto lo esto haciendo en neovim
+
