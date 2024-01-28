9a89fa8a03aee659de634e20e3ed8fd2bb9726c8
Benqui <bennyhoyos@hotmail.com>
2024-01-27 21:09:03 -0600

    creacion de: home, login, logout, registration in process

--- website/views.py
+++ website/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    # checar si tas logeado o no we 
+    if request.method=='POST':
+        username = request.POST['usrname']
+        passwd = request.POST['password']
+        #auth
+        user = authenticate(request, username=username, password=passwd)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "Tas loggeado bienvenido :) ")
+            return redirect('home')
+        else:
+            messages.success(request, "Error al loggearte quien ere we!! >:(")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+# def login_usr(request):
+#     pass
+
+def logout_usr(request):
+    logout(request)
+    messages.success(request, "Bye bye we vuelva pronto :)")
+    return redirect('home')
+
+
+def register_usr(request):
+    return render(request, 'register.html',{})
